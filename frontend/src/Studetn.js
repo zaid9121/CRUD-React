91ce61b0149fc9ebb1d0e26d865f7ecd50737722
zaid <alexsanderson610@gmail.com>
2023-09-04 19:06:26 +0200

    Hallo

--- frontend/src/Studetn.js
+++ frontend/src/Studetn.js
@@ -11,7 +11,7 @@ function Studetn() {
             .then(res => setStudent(res.data))
             .catch(err => console.log(err));
 
-    }, [])
+    }, []) 
 
     const handleDelete = async (id) => {
 
